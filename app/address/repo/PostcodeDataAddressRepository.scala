873977eb4bce0ee7a6974ee6fd1a49189832804b
Martijn Blankestijn <martijnblankestijn@gmail.com>
2016-02-05 11:07:13 +0100

    Removed retrieval of address

--- app/address/repo/PostcodeDataAddressRepository.scala
+++ app/address/repo/PostcodeDataAddressRepository.scala
@@ -8,12 +8,12 @@ import play.api.http.{ContentTypes, HeaderNames}
 import play.api.libs.json.{JsArray, JsLookupResult}
 import play.api.libs.ws.{WSClient, WSRequest}
 
+import scala.concurrent.Future
+
 /**
   * Documentation for API is http://www.postcodedata.nl/api/request/
-  *
-  * @param wsClient
   */
-class PostcodeDataAddressRepository @Inject()(wsClient: WSClient) extends AddressRepository {
+class PostcodeDataAddressRepository extends AddressRepository {
   implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
   private val postcodeUrl: String = "http://api.postcodedata.nl/v1/postcode/"
 
@@ -31,27 +31,6 @@ class PostcodeDataAddressRepository @Inject()(wsClient: WSClient) extends Addres
   }
 
   override def get(postalCode: String, houseNumber: Int) = {
-    Logger.debug(s"Retrieving address details from postal code $postalCode and house number $houseNumber")
-    val req: WSRequest = wsClient.url(postcodeUrl)
-      .withHeaders(HeaderNames.ACCEPT -> ContentTypes.JSON)
-      .withQueryString(
-        "streetnumber" -> houseNumber.toString,
-        "postcode" -> postalCode,
-        "ref" -> "somewhere.nl")
-
-    req.get()
-      .map(r =>
-        (r.json \ "status").as[String] match {
-          case "ok" =>
-            val firstAddress = (r.json \ "details").as[JsArray].head
-            Some(convert(firstAddress, houseNumber))
-          case "error" =>
-            Logger.info(s"Postal code $postalCode and $houseNumber returns error: ${r.json}")
-            (r.json \ "errormessage").as[String] match {
-              case "no results" => None
-              case _ => throw new IllegalStateException
-            }
-        }
-      )
+    Future.successful(None)
   }
 }
