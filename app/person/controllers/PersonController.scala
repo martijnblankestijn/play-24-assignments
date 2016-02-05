b6d7318463fe48633b4ef8f005358dc22b10acd2
Martijn Blankestijn <martijnblankestijn@gmail.com>
2016-02-05 14:20:19 +0100

    Basis starting point

--- app/person/controllers/PersonController.scala
+++ app/person/controllers/PersonController.scala
@@ -1,55 +1,11 @@
 package person.controllers
 
-import person.controllers.PersonJsonProtocol._
-import person.domain.Person
-import person.repo.MemoryPersonRepository
-import play.api.http.HeaderNames
-import play.api.libs.json.Json.toJson
-import play.api.libs.json._
-import play.api.mvc.{Action, BodyParsers, Controller}
+import person.repo.{PersonRepository, MemoryPersonRepository}
 
 
-class PersonController extends Controller {
-  val personRepo = MemoryPersonRepository
+class PersonController {
+  val personRepo: PersonRepository = MemoryPersonRepository
 
-  def persons() = Action {
-    Ok(toJson(personRepo.getAll))
-  }
-
-  def get(id: Long) = Action {
-    personRepo.get(id)
-      .map(p => Ok(toJson(p)))
-      .getOrElse(NotFound)
-  }
-
-  def saveNew() = Action(BodyParsers.parse.json) { request =>
-    val result: JsResult[Person] = request.body.validate[Person]
-    result.fold(
-      errors => InternalServerError(JsError.toJson(errors)),
-      p => {
-        val newPerson: Person = personRepo.saveNew(p)
-        Created(toJson(newPerson))
-          .withHeaders(
-            HeaderNames.LOCATION -> person.controllers.routes.PersonController.get(newPerson.id).url)
-      }
-    )
-  }
-
-  def update(id: Long) = Action(BodyParsers.parse.json) { request =>
-    val result: JsResult[Person] = request.body.validate[Person]
-    result.fold(
-      errors => InternalServerError(JsError.toJson(errors)),
-      person => {
-        personRepo.update(person.copy(id = id))
-        NoContent
-      }
-    )
-  }
-
-  def delete(id: Long) = Action {
-    personRepo.delete(id)
-    NoContent
-  }
 }
 
 
--- app/person/controllers/PersonJsonProtocol.scala
+++
@@ -1,8 +0,0 @@
-package person.controllers
-
-import person.domain.Person
-import play.api.libs.json.Json
-
-object PersonJsonProtocol {
-  implicit val personFormat = Json.format[Person]
-}
